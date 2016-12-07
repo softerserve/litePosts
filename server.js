36ddac64399ef4172545f553a4cc87d77bb60567
Bert McLees <bert.mclees@gmail.com>
2016-12-06 19:31:41 -0500

    Corrected problem with server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
@@ -10,4 +10,30 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port);
-console.log('Server started for Bert McLees - litePosts');
+console.log('Server started for Bert McLees - litePosts');*/
+
+//
+
+var express = require('express');
+
+// Create our app
+const PORT = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
